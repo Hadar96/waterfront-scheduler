20b31ac653f4a1a4ebe2e4bf2419d147a7f8db62
Your Name <you@example.com>
2025-03-31 16:04:18 +0300

    create store

---
+++ src/app/services/store.ts
@@ -0,0 +1,63 @@
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Lifeguard } from '../models/lifeguard';
+import { Activity } from '../models/activity';
+import { DayType } from '../models/daytype';
+
+export interface AppState {
+    lifeguards: Lifeguard[];
+    activities: Activity[];
+    daytypes: DayType[]; 
+    currentDayType: DayType | null;
+}
+
+const initialState: AppState = {
+    lifeguards: [],
+    activities: [],
+    daytypes: [],
+    currentDayType: null,
+};
+
+export class AppStore {
+    private state$ = new BehaviorSubject<AppState>(initialState);
+
+    // Get the current state as an observable
+    getState() {
+        return this.state$.asObservable();
+    }
+
+    /** Get the current state value */
+    getSnapshot() {
+        return this.state$.getValue();
+    }
+
+    // Update the state
+    updateState(partialState: Partial<AppState>) {
+        const currentState = this.state$.getValue();
+        this.state$.next({ ...currentState, ...partialState });
+    }
+
+    // Reset the state to the initial state
+    resetState() {
+        this.state$.next(initialState);
+    }
+
+    // Specific getters for new properties
+    getLifeguards() {
+        return this.getState().pipe(map(state => state.lifeguards));
+    }
+
+    getActivities() {
+        return this.state$.asObservable().pipe(map(state => state.activities));
+    }
+
+    getCurrentDayType() {
+        return this.state$.asObservable().pipe(map(state => state.currentDayType));
+    }
+
+    getAllDayTypes() {
+        return this.state$.asObservable().pipe(map(state => state.daytypes));
+    }
+}
+
+export const appStore = new AppStore();
